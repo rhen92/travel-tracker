7f3fc4f5b6b3968215b64f20e7aa07a0c2fc719a
Rachel Hendricks <rachelhen92@gmail.com>
2021-04-22 20:02:53 -0600

    Build TripRepository class

--- src/TripRepository.js
+++ src/TripRepository.js
@@ -1,14 +1,29 @@
-class Trip {
-  constructor(trip) {
-    this.id = trip.id;
-    this.userID = trip.userID;
-    this.destinationID = trip.destinationID;
-    this.travelers = trip.travelers;
-    this.date = trip.date;
-    this.duration = trip.duration;
-    this.status = trip.status;
-    this.suggestedActivities = [];
+class TripRepository {
+  constructor(tripData, destinationData) {
+    this.allTrips = tripData;
+    this.destinations = destinationData
+  }
+
+  findUsersTravel(id) {
+    return this.allTrips.filter(trip => trip.userID === id);
+  }
+
+  findTotalTripCostForUser(user) {
+    let travelerMoney = user.reduce((total, travel) => {
+      this.destinations.filter(trip => {
+        if (trip.id === travel.destinationID) {
+          if (new Date(travel.date).getFullYear() === 2021) {
+            let amountLodging = trip.estimatedLodgingCostPerDay * travel.duration;
+            let amountFlight = trip.estimatedFlightCostPerPerson * travel.travelers;
+            total += amountLodging += amountFlight;
+          }
+        }
+      });
+      return total;
+    }, 0);
+    let totalWithAgentFee = travelerMoney + (travelerMoney * 0.10);
+    return totalWithAgentFee;
   }
 }
 
-module.exports = Trip;
+module.exports = TripRepository;
