4630a738d6634fda12c60721620f391a0b4eaf9e
Rachel Hendricks <rachelhen92@gmail.com>
2021-04-24 11:49:37 -0600

    Update functions

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,6 @@
 import TripRepository from './TripRepository';
 import Trip from './Trip';
+import Traveler from './Traveler';
 
 let travelData;
 let destinationData;
@@ -13,6 +14,12 @@ const durationInput = document.querySelector('#duration');
 const travelerInput = document.querySelector('#numTravelers');
 const tripCost = document.querySelector('#tripCost');
 const tripForm = document.querySelector('#tripBooking');
+const usernameInput = document.querySelector('#usernameInput');
+const passwordInput = document.querySelector('#passwordInput');
+const loginButton = document.querySelector('#loginButton');
+const loginPage = document.querySelector('#loginPage');
+const mainPage = document.querySelector('#mainPage');
+let user;
 let tripRepository;
 
 const travelGET = () => fetch('http://localhost:3001/api/v1/trips')
@@ -33,6 +40,7 @@ const destinationGET = () => fetch('http://localhost:3001/api/v1/destinations')
 window.addEventListener('load', getData());
 submitButton.addEventListener('click', checkValidation);
 tripForm.addEventListener('input', createTrip);
+loginButton.addEventListener('click', checkLoginValidation);
 
 function getData() {
   travelGET();
@@ -41,7 +49,7 @@ function getData() {
 
 function showTrips() {
   tripRepository = new TripRepository(travelData.trips, destinationData.destinations);
-  let specificUserTravels = tripRepository.findUsersTravel(44);
+  let specificUserTravels = tripRepository.findUsersTravel(user.id);
   let individualInfo = specificUserTravels.map(travel => {
     destinationData.destinations.filter(trip => {
       if (trip.id === travel.destinationID) {
@@ -61,7 +69,7 @@ function showTrips() {
 }
 
 function showTotalSpentOnTrips() {
-  let specificUserTravels = tripRepository.findUsersTravel(44);
+  let specificUserTravels = tripRepository.findUsersTravel(user.id);
   let amountOfMoneySpent = tripRepository.findTotalTripCostForUser(specificUserTravels);
   totalSpent.innerText = `Total spent on trips this year: $${Math.round(amountOfMoneySpent)}`;
 }
@@ -98,7 +106,7 @@ function createTrip() {
   let currentDate = new Date(startCalendarInput.value);
   let trip = {
     id: tripRepository.findNextAvailableID(),
-    userID: 44,
+    userID: user.id,
     destinationID: tripRepository.findDestinationID(destinationDropdown.value),
     travelers: travelerInput.value,
     date: `${currentDate.getFullYear()}/${currentDate.getMonth() + 1}/${currentDate.getDate()}`,
@@ -123,4 +131,39 @@ function submitForm(trip) {
     }
   })
     .catch(err => err.message)
+}
+
+function checkLoginValidation() {
+  if (!passwordInput.value || !usernameInput.value) {
+    alert('Need username or password filled in.');
+  } else if (passwordInput.value !== 'travel2020') {
+    alert('Incorrect Password');
+  } else if (!usernameInput.value.includes('traveler')) {
+    alert('Incorrect Username');
+  } else if (usernameInput.value === 'traveler') {
+    alert('Incorrect Username');
+  } else {
+    checkForUser();
+  }
+}
+
+function checkForUser() {
+  event.preventDefault();
+  let specificUser = usernameInput.value.split('traveler');
+  if (specificUser[1] > 50 || specificUser[1] <= 0) {
+    alert('Incorrect Username')
+  } else {
+    fetch(`http://localhost:3001/api/v1/travelers/${specificUser[1]}`)
+      .then(response => response.json())
+      .then(data => user = new Traveler(data))
+      .then(data => showTrips())
+      .then(data => showTotalSpentOnTrips())
+      .catch(err => err.message);
+      hideLoginPage();
+  }
+}
+
+function hideLoginPage() {
+  loginPage.classList.add('hidden');
+  mainPage.classList.remove('hidden');
 }
