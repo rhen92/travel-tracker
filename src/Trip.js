eb7d1e40385b9898d43459d122e2570b7214b79f
Rachel Hendricks <rachelhen92@gmail.com>
2021-04-23 18:37:02 -0600

    Add findEstimatedCostOfTrip method

--- src/Trip.js
+++ src/Trip.js
@@ -9,6 +9,16 @@ class Trip {
     this.status = "pending";
     this.suggestedActivities = []
   }
+
+  findEstimatedCostOfTrip(data) {
+    let vacationSpot = data.destinations.find(trip => trip.id === this.destinationID);
+    let amountLodging = vacationSpot.estimatedLodgingCostPerDay * this.duration;
+    let amountFlight = vacationSpot.estimatedFlightCostPerPerson * this.travelers;
+    let total = 0;
+    total += amountLodging += amountFlight;
+    let totalWithAgentFee = total + (total * 0.10);
+    return totalWithAgentFee;
+  }
 }
 
 module.exports = Trip;
