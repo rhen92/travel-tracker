6bcc39d736966e9cd84e5a53651e88bec5a8c7f7
Rachel Hendricks <rachelhen92@gmail.com>
2021-04-26 13:51:46 -0600

    Remove commented out code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-// Do not delete or rename this file ********
 import './scripts.js';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-//import './images/turing-logo.png'
